bd39e72d1545f73b3f7e91fd7a14b57febdff3ef
lovehututu <18331998976@163.com>
2019-07-15 09:12:57 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react'
-import {Button, message} from 'antd'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 /*
 应用根组件
  */
-class App extends Component {
-
-  handleClick = () => {
-    message.success('成功啦...');
-  }
+export default class App extends Component {
 
   render() {
     return (
-      <Button type='primary' onClick={this.handleClick}>学习</Button>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Login}/>
+          <Route path='/admin' component={Admin}/>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
-
-export default App
